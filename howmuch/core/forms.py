2a45ba9e911c0980d8827ae80588e04905610675
kayethano <kayethano@gmail.com>
2012-10-07 21:48:24 -0500

    starting frontend

--- howmuch/core/forms.py
+++ howmuch/core/forms.py
@@ -1,5 +1,5 @@
 from django import forms
-from django.forms import ModelForm
+from django.forms import ModelForm, Textarea, TextInput, Select
 from django.contrib.admin import widgets   
 from howmuch.core.models import RequestItem, Proffer, Assignment
 from captcha.fields import CaptchaField
@@ -9,6 +9,17 @@ class RequestItemForm(ModelForm):
 	class Meta:
 		model = RequestItem
 		exclude = ('owner','date','pictures',)
+		widgets = {
+			'price' : TextInput(attrs={'class':'InputFormRequestItem',}),
+			'title' : TextInput(attrs={'class':'InputFormRequestItem', }),
+            'description': Textarea(attrs={'class':'InputFormRequestItem','cols': 40, 'rows': 10}),
+            'quantity' : TextInput(attrs={'class':'InputFormRequestItem',}),
+            'brand' : TextInput(attrs={'class':'InputFormRequestItem',}),
+            'model' : TextInput(attrs={'class':'InputFormRequestItem',}),
+            'state' : Select(attrs={'class':'InputFormRequestItem',}),
+            'daysLimit' : TextInput(attrs={'class':'InputFormRequestItem',}),
+            'addressDelivery' : Textarea(attrs={'class':'InputFormRequestItem', 'cols' : 40, 'rows' : '5'}),
+        }
 
 class ProfferForm(ModelForm):
 	class Meta:
