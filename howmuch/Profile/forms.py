0495e0c632ee94c9e78f0bbc15f903181eeab7eb
kayethano <kayethano@gmail.com>
2013-01-17 21:01:50 -0600

    remove Picture file

--- howmuch/Profile/forms.py
+++
@@ -1,8 +0,0 @@
-from django import forms
-from django.forms import ModelForm
-from howmuch.Profile.models import Profile
-
-class ProfileForm(ModelForm):
-    class Meta:
-        model = Profile
-        exclude = ('user', )
--- howmuch/Profile/urls.py
+++
@@ -1,5 +0,0 @@
-from django.conf.urls import patterns, include, url
-
-urlpatterns = patterns('howmuch.Profile.views',
-        url(r'^edit/$', 'edit' , name = "profileEdit"),
-) 
