d998d2c5c9c1bf56a859f0f00b7b0956668dad8c
kayethano <kayethano@gmail.com>
2013-01-26 02:19:26 -0600

    remove files in git

--- howmuch/items/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from howmuch.items.models import ItemsCatA, ItemsCatB, ItemsCatC
-
-admin.site.register(ItemsCatA)
-admin.site.register(ItemsCatB)
-admin.site.register(ItemsCatC)
--- howmuch/searchengine/urls.py
+++
@@ -1,6 +0,0 @@
-from django.conf.urls import patterns, include, url
-
-urlpatterns = patterns('howmuch.searchengine.views',
-        url(r'^$','searchservice', name = "searchservice"),
-
-) 
