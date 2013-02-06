5243e4608bd21bc3f65cae08fff71407baf75413
kayethano <kayethano@gmail.com>
2013-02-06 00:29:42 -0600

    purchases and sales templates done

--- howmuch/account/urls.py
+++ howmuch/account/urls.py
@@ -1,10 +1,6 @@
 from django.conf.urls import patterns, include, url
 
 urlpatterns = patterns('howmuch.account.views',
-        url(r'^purchases/published/$', 'publishedPurchases' , name = "publishedpurchases"),
-        url(r'^purchases/process/$', 'processPurchases' , name = "processpurchases"),
-        url(r'^purchases/completed/$', 'completedPurchases' , name = "completedpurchases"),
-        url(r'^sales/possible/$', 'possibleSales' , name = "possiblesales"),
-        url(r'^sales/process/$', 'processSales' , name = "processsales"),
-        url(r'^sales/completed/$', 'completedSales' , name = "completedsales"),
+        url(r'^purchases/$', 'purchases_list' , name = "purchases_list"),
+        url(r'^sales/$', 'sales_list' , name = "sales_list"),
 ) 
--- howmuch/account/views.py
+++ howmuch/account/views.py
@@ -1,56 +1,44 @@
 from django.contrib.auth.decorators import login_required
-from django.shortcuts import render_to_response
+from django.shortcuts import render_to_response, get_list_or_404
 from django.template import RequestContext
 
 from howmuch.article.models import Article, Offer, Assignment
 
-@login_required(login_url="/login/")
-def publishedPurchases(request):
-    items = Article.objects.filter(owner = request.user)
+@login_required(login_url='/login/')
+def purchases_list(request):
+    items = get_list_or_404(Article, owner=request.user)
     publishedPurchases = []
-    for item in items:
-        if not item.has_assignment():
-            publishedPurchases.append(item)
-    return render_to_response('account/publishedPurchases.html', {'publishedPurchases' : publishedPurchases }, context_instance = RequestContext(request))
-
-
-@login_required(login_url="/login/")
-def processPurchases(request):
-    items = Article.objects.filter(owner = request.user)
     processPurchases = []
-    for item in items:
-        if item.has_assignment() and not item.has_been_completed():
-            processPurchases.append(item)
-    return render_to_response('account/processPurchases.html', {'processPurchases' : processPurchases }, context_instance = RequestContext(request))
-
-
-@login_required(login_url="/login/")
-def completedPurchases(request):
-    items = Article.objects.filter(owner = request.user)
     completedPurchases = []
     for item in items:
-        if item.has_been_completed():
+        #Lista de compras publicadas
+        if not item.has_assignment():
+            publishedPurchases.append(item)
+        #Lista de compras en proceso
+        elif item.has_assignment() and not item.has_been_completed():
+            processPurchases.append(item)
+        #Lista de compras terminadas
+        elif item.has_been_completed():
             completedPurchases.append(item)
-    return render_to_response('account/completedPurchases.html', {'completedPurchases' : completedPurchases}, context_instance = RequestContext(request))
+    return render_to_response('account/purchases_list.html', 
+        {'publishedPurchases' : publishedPurchases, 'processPurchases' : processPurchases, 
+        'completedPurchases' : completedPurchases}, context_instance=RequestContext(request))
 
-@login_required(login_url="/login/")
-def possibleSales(request):
-    items = Offer.objects.filter(owner = request.user )
+@login_required(login_url='/login/')
+def sales_list(request):
+    items = get_list_or_404(Offer, owner=request.user)
+    #Ofertas realizadas
     possibleSales = []
     for item in items:
         if item.is_open():
             possibleSales.append(item)
-    return render_to_response('account/possibleSales.html', {'possibleSales' : possibleSales }, context_instance = RequestContext(request))
-
-@login_required(login_url="/login/")
-def processSales(request):
+    #Asignaciones 
     processSales = Assignment.objects.filter(owner = request.user, status__in = ["0","1","2","3"])
-    return render_to_response('account/processSales.html', {'processSales' : processSales}, context_instance = RequestContext(request))
-
-@login_required(login_url="/login/")
-def completedSales(request):
+    #Transacciones Terminadas
     completedSales = Assignment.objects.filter(owner = request.user, status = "4")
-    return render_to_response('account/completedSales.html', {'completedSales' : completedSales }, context_instance = RequestContext(request))
+    return render_to_response('account/sales_list.html',{'possibleSales' : possibleSales, 'processSales' : processSales,
+        'completedSales' : completedSales}, context_instance=RequestContext(request))
 
+    
 
 
