d1d518e7cff0b213a873483f17e6e1612cea33f2
kayethano <kayethano@gmail.com>
2013-03-26 11:26:34 -0600

    testing paypal ipn

--- howmuch/invoice/models.py
+++ howmuch/invoice/models.py
@@ -56,14 +56,14 @@ class Pay(models.Model):
 	def __unicode__(self):
 		return u'%s' % (self.invoice)
 
-	def create_pay(sender, instance, created, **kwargs):
-		from howmuch.invoice.models import Invoice
-		
-		if created:
-			invoice = Invoice.objects.get(invoice=str(instance.invoice))
-			pay = Pay.objects.create(invoice=invoice, amount=float(instance.mc_gross), reference=invoice.reference)
+	#def create_pay(sender, instance, created, **kwargs):
+	#	from howmuch.invoice.models import Invoice
 
-	post_save.connect(create_pay, sender=PayPalIPN)
+	#	if created:
+	#		invoice = Invoice.objects.get(invoice=str(instance.invoice))
+	#		pay = Pay.objects.create(invoice=invoice, amount=float(instance.mc_gross), reference=invoice.reference)
+
+	#post_save.connect(create_pay, sender=PayPalIPN)
 
 
 
